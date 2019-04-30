dc6d887dc47a020fa8e75877a96b9c652403cbc4
Colin Hogan <colinhogan7@gmail.com>
2019-04-30 00:20:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logShout(string){
 
 function sayHiToGrandma(string) {
 if (string.toLowerCase === string) {
-  return "I can\'t hear you!"
+  return "I can't hear you!"
 } else if (string == string.toUpperCase) {
   return "YES INDEED!"
 } else if (string === "I love you, Grandma.") {
